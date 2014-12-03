b50e8a7b725e4e3172cda7b029033ab75cfdf12a
wadadaaa <lopatinski@gmail.com>
2014-12-03 08:18:13 +0200

    Prepare to deploy on heroku

---
+++ djangogirls/settings/production.py
@@ -0,0 +1,14 @@
+import os
+from django.conf import settings
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+DATABASES = settings.DATABASES
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
